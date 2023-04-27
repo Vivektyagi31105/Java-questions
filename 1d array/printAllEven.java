9be9806edd458b761f8c4d03c9813a2febabda76
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-04-27 17:51:44 +0530

    Add files via upload

---
+++ 1d array/printAllEven.java
@@ -0,0 +1,11 @@
+//  tranverse over all the elements of the array using for each loop and print all even elements.
+public class printAllEven {
+    public static void main(String[] args) {
+        int arr[] ={34,21,64,65,43};
+        for(int elements : arr){
+            if(elements % 2 == 0){
+                System.out.println(elements);
+            }
+        }
+    }
+}
---
+++ 1d array/sumOfElements.java
@@ -0,0 +1,12 @@
+// sum of elements on even indices
+public class sumOfElements{
+    public static void main(String[] args) {
+        int arr[] ={3,20,4,6,9};
+
+         int sum=0;
+         for(int i=0; i<arr.length; i+=2){
+              sum+=arr[i];
+         }
+           System.out.println(sum);
+    }
+}
