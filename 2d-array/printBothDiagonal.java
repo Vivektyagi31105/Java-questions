9a9b0a0961adbd886f217345d13e13d165bf475b
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-02 02:09:21 +0530

    Add files via upload

---
+++ 2d-array/printBothDiagonal.java
@@ -0,0 +1,28 @@
+// write a program to print the elements of both the diagonals in a user inputted square matrix
+// in any order.
+import java.util.*;
+public class printBothDiagonal {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int m= scn.nextInt();
+        int n= scn.nextInt();
+             
+        int arr[][] = new int[m][n];
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        System.out.println("Both diagonal element: ");
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+            if(i+j==0 || i+j ==m-1|| i==j){
+                System.out.print(arr[i][j]+" ");
+            }
+            }
+        }
+
+
+    }
+}
