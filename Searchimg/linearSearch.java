c0a9ec6603b41ec7e629a9c6dfee3b8774218de9
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-03 02:18:23 +0530

    Add files via upload

---
+++ Searchimg/linearSearch.java
@@ -0,0 +1,33 @@
+// Q1. Given an array. FInd the number X in the array. If the element is present, return the index of the element,
+// else print “Element not found in array”. Input the size of array, array from user and the element X from user.
+// Use Linear Search to find the element.
+
+import java.util.*;
+public class linearSearch {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Input number of element in array:");
+    int n= scn.nextInt();
+    System.out.println("Input array elements:");
+    int arr[] = new int[n];
+    for(int i=0; i<n; i++){
+        arr[i]= scn.nextInt();
+    }
+    System.out.println("Input target element:");
+    int x= scn.nextInt();
+
+    int idx=-1;
+    for(int i=0; i<n; i++){
+        if(arr[i]==x){
+            idx=i;
+            break;
+        }
+    }
+    if(idx==-1){
+        System.out.println("Element not found in array");
+    }else{
+        System.out.println("Element found on index: "+ idx);
+    }
+  }
+    
+}
