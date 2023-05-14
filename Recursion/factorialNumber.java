3fdfe571f14e89f0b081ea0f2e50d5437ea1be77
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-14 23:25:59 +0530

    Add files via upload

---
+++ Recursion/factorialNumber.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class factorialNumber{
+    static int findFactorial(int n){
+        int result= 0;
+        if(n==0 || n==1){
+            return 1;
+        }else{
+            result= n*findFactorial(n-1);
+                return result;
+            
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Type the number:");
+        int n= scn.nextInt();
+
+        System.out.println("Factorial is:" + findFactorial(n));
+
+    }
+}
+        
+        
