d90fb03968e12a9f3d2571a9ae9bc0f48d965305
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-15 23:14:03 +0530

    Add files via upload

---
+++ Recursion/fibonacciSeries.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class fibonacciSeries {
+    static int findFibonacci(int n){
+      int result=0;
+      if(n<=1){
+        return n;
+      }else{
+        result= findFibonacci(n-1)+ findFibonacci(n-2);
+      }
+      return result;
+    }
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        System.out.println("Type the number:");
+        int n= scn.nextInt();
+
+       System.out.println("Result for fibonacci series is: "+findFibonacci(n));
+
+    }
+}
