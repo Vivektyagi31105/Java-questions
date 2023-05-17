b01a4687ee94463d3ad8a5fbe4a5676fb6e6cad4
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-17 16:47:37 +0530

    Add files via upload

---
+++ Recursion/maxValueOfAnArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class maxValueOfAnArray{
+    static int maxValue(int arr[], int n){
+        
+            if(n==1){
+                return arr[0];
+            }else{
+                return Math.max(arr[n-1],maxValue(arr, n-1));
+            }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Type the number of elements in an array:");
+        int n= scn.nextInt();
+        System.out.println("Type the elements of an array:");
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]= scn.nextInt();
+
+        }
+        System.out.println("Max element in an array is:"+maxValue(arr, n));
+
+
+    }
+}
