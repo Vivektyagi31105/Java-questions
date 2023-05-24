e3c6d4456871f2fb67b95d9a14b50b12cdb0fd36
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-24 23:40:02 +0530

    Update largestElement.java

--- 2d-array/largestElement.java
+++ 2d-array/largestElement.java
@@ -7,6 +7,7 @@ public class largestElement {
         Scanner scn = new Scanner(System.in);
         int m=scn.nextInt();
         int n= scn.nextInt();
+        
         int arr[][] = new int[m][n];
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
