9cde846eba58ff8499a9814dd3fba381988f0c1b
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-06-12 23:59:40 +0530

    Update aboveSeconaryDiagonal.java

--- 2d-array/aboveSeconaryDiagonal.java
+++ 2d-array/aboveSeconaryDiagonal.java
@@ -17,6 +17,7 @@ public class aboveSeconaryDiagonal {
         }
         
         System.out.println("Elements above secondary diagonal:");
+        
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
     
