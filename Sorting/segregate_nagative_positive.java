d2441352b158461984f7c9e88ff35baab8f9c76f
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-06-22 23:44:16 +0530

    Add files via upload

---
+++ Sorting/segregate_nagative_positive.java
@@ -0,0 +1,75 @@
+public class segregate_nagative_positive {
+    static void segregate(int arr[], int temp[],int low, int mid, int high){
+        int i=0, j=0, k=low;
+
+        // copy negative elements from left subarray
+        for(i= low; i<=mid; i++){
+            if(arr[i]<0){
+                temp[k++]= arr[i];
+            }
+        }
+        // copy negative elements from right subarray
+        for(j= mid+1; j<=high; j++){
+            if(arr[j]<0){
+                temp[k++]= arr[j];
+            }
+        }
+
+        // copy positive elements from left subarray
+        for(i= low; i<= mid; i++){
+            if(arr[i]>= 0){
+                temp[k++]= arr[i];
+            }
+        }
+        // copy positive elements from right subarray
+        for(j= mid+1; j<= high; j++){
+            if(arr[j]>=0){
+                temp[k++]= arr[j];
+            }
+        }
+        // copy back to original array
+        for(i= low; i<= high; i++){
+            arr[i]= temp[i];
+        }
+
+    }
+    static void partition(int arr[],int temp[],int low,int high){
+        if(high<=low){
+            return;
+        }
+        
+        // mid of array
+        int mid= low+(high-low)/2;
+
+        // split the left subarray
+        partition(arr, temp, low, mid);
+
+        // spllt the right subarray
+        partition(arr, temp, mid+1, high);
+
+        //  combine left and right subarray
+        segregate(arr,temp,low,mid,high);
+
+    }
+    static void print(int arr[],int n){
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] ={19, -20, 7, -4, -13, 11, -5, 3};
+        int n= arr.length;
+        int temp[] = new int[n];
+        for(int i=0; i<n; i++){
+            temp[i] = arr[i];
+        }
+        System.out.println("Before segregate array: ");
+        print(arr, n);
+
+        partition(arr, temp, 0, n-1);
+
+        System.out.println("After segregate array: ");
+        print(arr, n);
+    }
+}
