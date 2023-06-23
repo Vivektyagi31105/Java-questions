15beb11673adf4e50b1b5d6c869962ecd88c0bdc
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-06-23 22:44:42 +0530

    Add files via upload

---
+++ Sorting/segregate.java
@@ -0,0 +1,33 @@
+public class segregate {
+    static void segregateArray(int arr[],int n){
+        int pIndex= 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i]<0){
+                int swap = arr[i];
+                arr[i] = arr[pIndex];
+                arr[pIndex]= swap;
+                pIndex++;
+            }
+
+        }
+
+    }
+    static void print(int arr[], int n){
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] ={19, -20, 7, -4, -13, 11, -5, 3};
+        int n= arr.length;
+        System.out.println("Before segregate array: ");
+        print(arr, n);
+       
+        segregateArray(arr,n);
+        System.out.println("After segregate: ");
+        print(arr, n);
+      
+    }
+}
