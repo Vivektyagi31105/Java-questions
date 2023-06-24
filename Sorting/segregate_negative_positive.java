a08ac3b6630b9a9d3c74cf7d7f052c8b4bf2ccb6
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-06-24 23:24:34 +0530

    Update segregate_negative_positive.java

--- Sorting/segregate_negative_positive.java
+++ Sorting/segregate_negative_positive.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class segregate_negative_positive {
     
-    static void segregate(int arr[], int temp[],int low, int mid, int high){
+   public static void segregate(int arr[], int temp[],int low, int mid, int high){
         int i=0, j=0, k=low;
 
         // copy negative elements from left subarray
