09014c1c57455424dbd14214aecaed3b81c351cb
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-06-26 23:03:57 +0530

    Update peakElement.java

--- 1d array/peakElement.java
+++ 1d array/peakElement.java
@@ -1,6 +1,7 @@
 //  find peak element. A peak element which is greater than left and right.
 
 import java.util.*;
+
 public class peakElement {
     public static void main(String[] args) {
         int arr[] = {1,3,2,6,5};
