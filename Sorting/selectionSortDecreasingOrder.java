b9b98db85aa43ac27099bce9a92e80886de16b4d
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-06-28 23:39:46 +0530

    Update selectionSortDecreasingOrder.java

--- Sorting/selectionSortDecreasingOrder.java
+++ Sorting/selectionSortDecreasingOrder.java
@@ -5,6 +5,7 @@
 import java.util.*;
 
 public class selectionSortDecreasingOrder {
+    
     public static void selectionSort(int arr[]){
         for(int i=0; i<arr.length; i++){
             int min_idx= i;
