ebf86b15e794cc467d3a09522c2f6d9f1b8e835f
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-06-29 23:36:09 +0530

    Update kth_smallestElement.java

--- Sorting/kth_smallestElement.java
+++ Sorting/kth_smallestElement.java
@@ -1,6 +1,7 @@
 import java.util.*;
+
 public class kth_smallestElement {
-     static int  partition(int arr[],int low, int high){
+     public static int  partition(int arr[],int low, int high){
         int pivot = arr[low];
         int i= low;
    
