8a660028b522dd2f86f43c340f12dec0ec53ee29
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-06-30 23:18:37 +0530

    Add files via upload

---
+++ Sorting/sumOfCorrespondingElement.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class sumOfCorrespondingElement {
+    static boolean correspondingSum(int []a,Integer b[], int n, int k){
+          if(n!=b.length){
+            return false;
+          }
+         
+          Arrays.sort(a);
+            Arrays.sort(b,Collections.reverseOrder());
+       
+        
+          for(int i=0; i<n; i++)
+            if(a[i]+ b[i]<k)
+                return false;
+          
+          return true;
+    }
+    public static void main(String[] args) {
+        int a[]= {2,1,3};
+        Integer b[]= {7,8,9};
+
+        int n= a.length;
+     
+        int k=10;
+
+        if(correspondingSum(a, b, n, k)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
