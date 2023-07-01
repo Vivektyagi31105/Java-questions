c5945bc0cadd85c32566bf302c1b297089b4d5ff
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-01 22:36:34 +0530

    Update bubbleSort1.java

--- Sorting/bubbleSort1.java
+++ Sorting/bubbleSort1.java
@@ -19,7 +19,7 @@ public static void main(String[] args) {
      for(int i=0; i<n-1; i++){
         for(int j=0; j<n-i-1; j++){
             if(arr[j]>arr[j+1]){
-                // < for decreasing order
+                // [<] for decreasing order
                 int swap= arr[j];
                 arr[j]= arr[j+1];
                 arr[j+1]= swap;
