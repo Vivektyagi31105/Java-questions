1ae8fde19f276ef84cf88a4037ccfb5492b9d03a
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-03 23:55:55 +0530

    Update printAllEven.java

--- 1d array/printAllEven.java
+++ 1d array/printAllEven.java
@@ -1,5 +1,7 @@
 //  traverse over all the elements of the array using for each loop and print all even elements.
+
 import java.util.*;
+
 public class printAllEven {
     public static void main(String[] args) {
         int arr[] ={34,21,64,65,43};
