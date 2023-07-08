89dc0ef3314b4e806ba38f4adaec98a4455cd5ae
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-08 23:40:47 +0530

    Update insertionSort.java

--- Sorting/insertionSort.java
+++ Sorting/insertionSort.java
@@ -5,7 +5,9 @@ import java.util.*;
 
 public class insertionSort {
     public static void insertion_Sort(int arr[]){
+        
         for(int i=1; i<arr.length; i++){
+            
             int j=i;
 
             while(j>0 && arr[j]<arr[j-1]){
@@ -20,8 +22,10 @@ public class insertionSort {
     }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        
         System.out.println("Type the number of elements in an array:");
         int n= scn.nextInt();
+        
           System.out.println("Type the number of elements in an array:");
           int arr[]= new int[n];
           for(int i=0; i<n; i++){
